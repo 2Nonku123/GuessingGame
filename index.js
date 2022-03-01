13467ff050dae63f9d4dae7b99bc7aa4103c30e4
2Nonku123 <t33tndlovu@gmail.com>
2022-03-01 13:07:04 +0200

    commit

--- index.js
+++ index.js
@@ -1,43 +1,59 @@
-// const randomElem = document.querySelector(".random");
-
-let randomNumber = Math.ceil((Math.random() * 100))
-
-// randomElem.innerHTML = randomNumber;
-
-const number_guess = document.querySelector(".number_guess");
-const guess_btn = document.querySelector(".guess_btn");
-const message = document.querySelector(".message");
-
-/*
-If the guess is too low show : 
-
-* "Your guess is too low"
-
-Or if too high show the message 
-* "Your guess is too high" 
-If the correct number is guessed show this message: 
-* "Correct, the secret number is ${number}".
-
-*/
+let randomNumber = Math.ceil(Math.random()* 100)
 
 
-function guessBtnClicked() {
-	const guessedNo = Number(number_guess.value);
-
-	if (guessedNo < randomNumber) {
-		message.innerHTML = "Your guess is too low";
-	} else if (guessedNo > randomNumber) {
-		message.innerHTML = "Your guess is too high";
-	} else {
-		message.innerHTML =  `Correct, the secret number is ${guessedNo}`;
-	}
 
 
-	setTimeout(function(){
-		message.innerHTML = "";
-	}, 2000);
+const GuessNumber = document.querySelector('.guessNumber')
+const btnGuess = document.querySelector('.btnGuess')
+const btnRefresh = document.querySelector('.btnRefresh')
+const message = document.querySelector('.message')
 
 
+
+
+function btnClicked() {
+   
+    const guessedNo = Number(GuessNumber.value);
+
+    if( guessedNo < randomNumber){
+      message.innerHTML = "Your guess is too low";
+      }
+      else if( guessedNo > randomNumber){
+        message.innerHTML = "Your guess is too high";
+      }else{
+        message.innerHTML = `Correct, the secret number is ${guessedNo}`;
+        randomNumber = Math.ceil(Math.random()* 100)
+        count = -1;
+        setTimeout(function() { alert("New game starting!"); }, 100);
+      }
+
+      if (guessedNo > 100){
+        message.innerHTML = "Error, Number Cannot be bigger than 100";
+    }
+    else if (guessedNo < 0){
+        message.innerHTML = "Error, Number Cannot be smaller than 0";
+    }
+
+    
+
+    
+    
+
+     
+      setTimeout(function(){
+        message.innerHTML = ""
+      }, 3000);
+     
+ 
 }
 
-guess_btn.addEventListener('click', guessBtnClicked)
+function refreshBtnClicked (){
+  randomNumber = Math.ceil(Math.random()* 100)
+
+
+  alert("New Number Generated!");
+}
+
+
+    btnGuess.addEventListener('click', btnClicked)
+    btnRefresh.addEventListener('click', refreshBtnClicked)
